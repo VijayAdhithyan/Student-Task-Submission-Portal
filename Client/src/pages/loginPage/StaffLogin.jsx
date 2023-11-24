e32d8a75b1ad7b8dc19043be4eb882df96e636d7
VijayAdhithyan <vijayadhithyan14@gmail.com>
2023-11-24 18:34:18 +0530

    staff login and signup updated

--- Client/src/pages/loginPage/StaffLogin.jsx
+++ Client/src/pages/loginPage/StaffLogin.jsx
@@ -1,21 +1,49 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { staffLogin } from "../../actions/auth";
 
 const StaffLogin = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!email) {
+      alert("Enter Email");
+    }
+    if (!password) {
+      alert("Enter Password");
+    }
+
+    dispatch(staffLogin({ email, password }, navigate));
+  };
   return (
     <div className="login-session">
       <h1>Staff Login Page</h1>
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <div className="inp">
           <label htmlFor="">E-Mail ID</label>
-          <input type="text" placeholder="mail Id" />
+          <input
+            type="text"
+            placeholder="mail Id"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="inp">
           <label htmlFor="">Password</label>
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button className="login-btn">Login</button>
         <br />
-        <Link to='/auth/signup/staff' className="signup-btn">
+        <Link to="/auth/signup/staff" className="signup-btn">
           Sign up
         </Link>
       </form>
--- Client/src/pages/signupPage/StaffSignupPage.jsx
+++ Client/src/pages/signupPage/StaffSignupPage.jsx
@@ -1,22 +1,61 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { staffSignup } from "../../actions/auth";
 
 
 const StaffSignupPage = () => {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      alert("Enter Name");
+    }
+    if (!email) {
+      alert("Enter Email");
+    }
+    if (!password) {
+      alert("Enter Password");
+    }
+
+    dispatch(staffSignup({ name, email, password }, navigate));
+  };
+
   return (
     <div className="login-session">
       <h1>Staff Sign up Page</h1>
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <div className="inp">
           <label htmlFor="">Name</label>
-          <input type="text" placeholder="name" />
+          <input
+            type="text"
+            placeholder="name"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="inp">
           <label htmlFor="">E-Mail ID</label>
-          <input type="text" placeholder="mail Id" />
+          <input
+            type="text"
+            placeholder="mail Id"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="inp">
           <label htmlFor="">Password</label>
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button
           to="/auth/signup/staff"
