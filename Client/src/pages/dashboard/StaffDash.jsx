af50a5f12a9ea3a9aea06c16566c212976942e59
VijayAdhithyan <vijayadhithyan14@gmail.com>
2023-11-28 05:46:03 +0530

    UI changes in staff dashboard

--- Client/src/pages/dashboard/StaffDash.jsx
+++ Client/src/pages/dashboard/StaffDash.jsx
@@ -1,11 +1,76 @@
-
+import { IoMdAdd } from "react-icons/io";
+import "./Dashboard.css";
 
 const StaffDash = () => {
   return (
-    <div>
-      <h1>Staff Dashboard</h1>
-    </div>
-  )
-}
+    <>
+      <h1 className="dash-heading">Staff Dashboard</h1>
+      <div className="task-btn">
+        <IoMdAdd className="add-icon" />
+        <p>New Task</p>
+      </div>
+      <div className="created-task">
+        <p>Created Task :</p>
+        <div className="task-container">
+          <div className="task-details">
+            <p className="task-heading">
+              Create an user DashBoard using an react js for frontend UI
+            </p>
+            <p className="task-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              natus impedit harum aut sint, ipsam nisi fuga porro accusamus,
+              omnis nam doloribus corrupti adipisci dolores atque labore rerum
+              doloremque culpa?
+            </p>
+          </div>
+          <div className="task-details">
+            <p className="task-heading">
+              Create an user DashBoard using an react js for frontend UI
+            </p>
+            <p className="task-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              natus impedit harum aut sint, ipsam nisi fuga porro accusamus,
+              omnis nam doloribus corrupti adipisci dolores atque labore rerum
+              doloremque culpa?
+            </p>
+          </div>
+          <div className="task-details">
+            <p className="task-heading">
+              Create an user DashBoard using an react js for frontend UI
+            </p>
+            <p className="task-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              natus impedit harum aut sint, ipsam nisi fuga porro accusamus,
+              omnis nam doloribus corrupti adipisci dolores atque labore rerum
+              doloremque culpa?
+            </p>
+          </div>
+          <div className="task-details">
+            <p className="task-heading">
+              Create an user DashBoard using an react js for frontend UI
+            </p>
+            <p className="task-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              natus impedit harum aut sint, ipsam nisi fuga porro accusamus,
+              omnis nam doloribus corrupti adipisci dolores atque labore rerum
+              doloremque culpa?
+            </p>
+          </div>
+          <div className="task-details">
+            <p className="task-heading">
+              Create an user DashBoard using an react js for frontend UI
+            </p>
+            <p className="task-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              natus impedit harum aut sint, ipsam nisi fuga porro accusamus,
+              omnis nam doloribus corrupti adipisci dolores atque labore rerum
+              doloremque culpa?
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default StaffDash
+export default StaffDash;
